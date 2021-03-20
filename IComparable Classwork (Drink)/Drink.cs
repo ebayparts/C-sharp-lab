12d3d1bc08f0a0eeee3527322fb17f4e54052cac
ebayparts <sanyav89@ukr.net>
2021-03-20 05:31:34 +0200

    Adding lab work IComparable Interface

---
+++ IComparable Classwork (Drink)/Drink.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IComparable_Classwork__Drink_
+{
+    public enum DrinkType { Sparkling, Still };
+    class Drink : IComparable, IComparable<Drink>, IEquatable<Drink>
+    {
+        public string DrinkName { get; set; }
+        public DrinkType DrinkType { get; set; }
+        public string Manufacturer { get; set; }
+        public int KkalQty { get; set; }
+        public int Price { get; set; }
+        public Drink(string drinkName, DrinkType drinkType, string manufacturer, int kkalQty, int price)
+        {
+            DrinkName = drinkName;
+            DrinkType = drinkType;
+            Manufacturer = manufacturer;
+            KkalQty = kkalQty;
+            Price = price;
+
+        }
+        public override string ToString()
+        {
+            return $"Drink: {DrinkName},\tDrink type: {DrinkType},\tManufacturer: {Manufacturer},\tCalorific value: {KkalQty},\tPrice: {Price}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            //if (this.DrinkType > ((Drink)obj).DrinkType)
+            //    return -1;
+            if (this.DrinkType == ((Drink)obj).DrinkType)
+            {
+                return this.DrinkName.CompareTo(((Drink)obj).DrinkName);
+            }
+            return this.DrinkType.CompareTo(((Drink)obj).DrinkType);
+        }
+
+        public int CompareTo(Drink other)
+        {
+            if (this.DrinkType == (other).DrinkType)
+            {
+                return this.DrinkName.CompareTo(other.DrinkName);
+            }
+            return this.DrinkType.CompareTo(other.DrinkType);
+        }
+
+        public bool Equals(Drink other)
+        {
+            if (other == null)
+                return false;
+            if (this.DrinkName == other.DrinkName)
+                return true;
+            else
+                return false;
+        }
+    }
+    class CompareByPriceAsc : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            return ((Drink)x).Price.CompareTo(((Drink)y).Price);
+        }
+    }
+    class CompareByPriceDesc : IComparer<Drink>
+    {
+        public int Compare(Drink x, Drink y)
+        {
+            return -x.Price.CompareTo(y.Price);
+        }
+    }
+    class CompareByKkalDesc : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            return -((Drink)x).KkalQty.CompareTo(((Drink)y).KkalQty);
+        }
+    }
+    class CompareByKkalAsc : IComparer<Drink>
+    {
+        public int Compare(Drink x, Drink y)
+        {
+            return x.KkalQty.CompareTo(y.KkalQty);
+        }
+    }
+    class CompareByManDesc : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            return -((Drink)x).Manufacturer.CompareTo(((Drink)y).Manufacturer);
+        }
+    }
+    class CompareByManAsc : IComparer<Drink>
+    {
+        public int Compare(Drink x, Drink y)
+        {
+            return x.Manufacturer.CompareTo(y.Manufacturer);
+        }
+    }
+}
---
+++ IComparable Classwork (Drink)/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IComparable_Classwork__Drink_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Drink> drinks = new List<Drink>
+            {
+            new Drink("Morshinka", (DrinkType)1, "Morshin", 30, 10),
+            new Drink("Pepsi", (DrinkType)0, "Pepsi-Cola", 50, 30),
+            new Drink("Sprite", (DrinkType)0, "Coca-Cola", 40, 35),
+
+            new Drink("Gorilka", (DrinkType)1, "Gorilka factory", 150, 80)
+        };
+            foreach (var item in drinks)
+            {
+                Console.WriteLine(item);
+            }
+            drinks.Sort();
+            Console.WriteLine("After sorting:");
+            foreach (var item in drinks)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("-------------------Using Compare Object)--------------");
+            CompareByPriceAsc a = new CompareByPriceAsc();
+            drinks.Sort(a.Compare);
+            Console.WriteLine("After sorting ByPriceAsc:");
+            foreach (var item in drinks)
+            {
+                Console.WriteLine(item);
+            }
+
+            CompareByKkalDesc b = new CompareByKkalDesc();
+            drinks.Sort(b.Compare);
+            Console.WriteLine("After sorting ByKkalDesc:");
+            foreach (var item in drinks)
+            {
+                Console.WriteLine(item);
+            }
+
+            CompareByManAsc c = new CompareByManAsc();
+            drinks.Sort(c.Compare);
+            Console.WriteLine("After sorting ByManAsc:");
+            foreach (var item in drinks)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("-------------------Using Compare Drink)--------------");
+            drinks.Sort(new CompareByPriceDesc());
+            Console.WriteLine("After sorting ByPriceDesc:");
+            foreach (var item in drinks)
+            {
+                Console.WriteLine(item);
+            }
+
+            drinks.Sort(new CompareByKkalAsc());
+            Console.WriteLine("After sorting ByKkalAsc:");
+            foreach (var item in drinks)
+            {
+                Console.WriteLine(item);
+            }
+
+            drinks.Sort(new CompareByManAsc());
+            Console.WriteLine("After sorting ByManAsc:");
+            foreach (var item in drinks)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"-----------------Checking IEquatable interface implementation: ------------");
+            drinks.Add(new Drink("Fanta", (DrinkType)1, "Coca-Cola", 15,22));
+            if (drinks[0].Manufacturer.Equals(drinks[4].Manufacturer))
+                Console.WriteLine($"Manufacturer \"{drinks[0].Manufacturer}\" is EQUAL to \"{drinks[4].Manufacturer}\"");
+            else
+                Console.WriteLine($"Manufacturer \"{drinks[0].Manufacturer}\" is NOT EQUAL to \"{drinks[4].Manufacturer}\"");
+
+
+        }
+
+    }
+}
