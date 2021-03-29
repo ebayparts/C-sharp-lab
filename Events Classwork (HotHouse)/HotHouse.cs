c4d30cec57a26751aee6d94b8126ec91db863344
ebayparts <sanyav89@ukr.net>
2021-03-29 18:07:02 +0300

    adding Lab work: Events (HotHouse)

---
+++ Events Classwork (HotHouse)/HotHouse.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_Classwork__HotHouse_
+{
+    delegate void HotHouseDeleg(HotHouse house);
+    class HotHouse
+    {
+        public readonly int maxT = 23, minT = 17;
+        public string Title { get; set; } = "Unsigned";
+        int temperature = 15;
+        public event HotHouseDeleg TooHot;
+        public event HotHouseDeleg TooCold;
+        public event HotHouseDeleg Well;
+        public int Temperature
+        {
+            get => temperature;
+            set
+            {
+                temperature = value;
+                if (value > maxT)
+                    TooHot?.Invoke(this);
+                else if (value < minT)
+                    TooCold?.Invoke(this);
+                else
+                    Well?.Invoke(this);
+            }
+        }
+        public override string ToString()
+        {
+            return $"\nNew Temp: {Temperature}";
+        }
+    }
+    class Heater
+    {
+        public string Name { get; set; } = "Unsigned";
+        public void Warm(HotHouse h)
+        {
+            Console.WriteLine($"-------------Heater works. (Temp: {h.Temperature} is lower than minimum: {h.minT})");
+            h.Temperature += 5;
+        }
+    }
+    class Cooler
+    {
+        public void Cool(HotHouse h)
+        {
+            Console.WriteLine($"-------------Cooler works. (Temp: {h.Temperature} is higher than maximum: {h.maxT})");
+            h.Temperature -= 5;
+        }
+    }
+    class Well
+    {
+        public void Ok(HotHouse h)
+        {
+            Console.WriteLine($"Temperature is OK ({h.Temperature})");
+        }
+    }
+}
---
+++ Events Classwork (HotHouse)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Events_Classwork__HotHouse_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Heater heater = new Heater { };
+            Cooler cooler = new Cooler { };
+            Well well = new Well { };
+            HotHouse hotHouse = new HotHouse { };
+            hotHouse.TooCold += heater.Warm;
+            hotHouse.TooHot += cooler.Cool;
+            hotHouse.Well += well.Ok;
+            hotHouse.Temperature = 24;
+            Console.WriteLine("--------------------Nature influence imitation:------------------");
+            for (int i = 0; i < 30; i++)
+            {
+                //Console.ReadKey(); // <- I thought it will be faster to use "Sleep" method than press any button 30 times:
+                System.Threading.Thread.Sleep(200);
+                Random random = new Random();
+                hotHouse.Temperature += random.Next(-5, 5);
+            }
+        }
+    }
+}
